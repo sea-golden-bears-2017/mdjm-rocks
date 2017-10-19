3e88792e586697e71dd89c26abb257527d7938ae
Marlo Ito & Miguel Angel Ibarra, Jr <pair+marloito.migaibarra@devbootcamp.com>
2017-10-18 19:41:16 -0700

    drying up code

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,6 +2,7 @@ class PartsController < ApplicationController
   def index
     if params[:name]
       @warehouse = Warehouse.find_by(name: params[:name])
+      @parts = @warehouse.parts.uniq
     else
       @parts = Part.all
     end
