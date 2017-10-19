a9b22562fd0fc5bc5aa5a68d67df9150494d8ba9
Devin Miller & Marlo Ito <pair+devzmiller.marloito@devbootcamp.com>
2017-10-19 15:58:31 -0700

    Add warehouses#create logic and handle errors

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -4,5 +4,19 @@ class WarehousesController < ApplicationController
   end
 
   def create
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save
+      @parts = @warehouse.parts.uniq
+      render "parts/index"
+    else
+      @errors = @warehouse.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name)
   end
 end
