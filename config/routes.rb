33cb06bf9b727f478feab4557f253990649bfea2
Miguel Angel Ibarra, Jr. & John Simoni <pair+Migaibarra.jsimoni42@devbootcamp.com>
2017-10-19 11:57:25 -0700

    removed comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
   resources :sessions, only: [:new, :create, :destroy]
-  # delete "/sessions", to: 'sessions#destroy'
   resources :parts, only: [:index]
   resources :warehouses, only: [:find] do
     resources :parts, only: [:index]
