0097ccc2d5c42452dfa5a4b23c7cfcd305a542ec
Devin Miller & Marlo Ito <pair+devzmiller.marloito@devbootcamp.com>
2017-10-19 15:25:07 -0700

    Add routes for new warehouses

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "sessions#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :parts, only: [:index]
-  resources :warehouses, only: [:find] do
+  resources :warehouses, only: [:new, :create] do
     resources :parts, only: [:index]
   end
 end
