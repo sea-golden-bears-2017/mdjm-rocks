afe4fc521aeba42d6e9e6f5ca473014e6a15ed1a
Devin Miller & Marlo Ito <pair+devzmiller.marloito@devbootcamp.com>
2017-10-19 19:08:17 -0700

    add association b/w warehouses and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :name, :employee_num, :password, :role, presence: true
   # validates :password_digest, presence: true, on: :create, unless: 'password.nil? || password.empty?'
   validates :employee_num, uniqueness: true
+  belongs_to :warehouse
   has_secure_password
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,6 @@
 class Warehouse < ApplicationRecord
   validates :name, presence: true
   has_many :warehouses_parts
+  has_many :users
   has_many :parts, through: :warehouses_parts
 end
