607ff1ca7b0bc86e246825e03fd16391f9f36fc3
Devin Miller & Marlo Ito <pair+devzmiller.marloito@devbootcamp.com>
2017-10-19 20:42:14 -0700

    Fix warehouses#create tests to account for manager auth. All tests passing

--- spec/controllers/warehouses_controller_spec.rb
+++ spec/controllers/warehouses_controller_spec.rb
@@ -25,34 +25,44 @@ describe WarehousesController, type: :controller do
 
   describe '#create' do
     before(:each) do
-      post :create, params: {warehouse: {name: "Seattle"}}
+      post :create, params: {warehouse: {name: "Seattle"}}, session: {user_id: manager.id}
     end
-    it 'sets an instance variable of a warehouse' do
-      expect(assigns[:warehouse].name).to eq "Seattle"
-    end
-    it 'returns an OK status' do
-      expect(response).to be_ok
+    context "when user is a manager" do
+      it 'sets an instance variable of a warehouse' do
+        expect(assigns[:warehouse].name).to eq "Seattle"
+      end
+      it 'returns an OK status' do
+        expect(response).to be_ok
+      end
+
+      context 'when warehouse is valid' do
+        it 'assigns a parts variable' do
+          expect(assigns[:parts]).to be_a Array
+        end
+        it 'renders the parts index show page' do
+          expect(response).to render_template("parts/index")
+        end
+      end
+
+      context 'when warehouse is invalid' do
+        before(:each) do
+          post :create, params: {warehouse: {name: nil}}, session: {user_id: manager.id}
+        end
+        it 'assigns an errors variable' do
+          expect(assigns[:errors]).to_not be_empty
+        end
+        it 'renders the new warehouse view' do
+          expect(response).to render_template(:new)
+        end
+      end
     end
 
-    context 'when warehouse is valid' do
-      it 'assigns a parts variable' do
-        expect(assigns[:parts]).to be_a Array
-      end
-      it 'renders the parts index show page' do
-        expect(response).to render_template("parts/index")
+    context 'when user is not a manager' do
+      it "renders 404 page" do
+        post :create, params: {warehouse: {name: "Seattle"}}, session: {user_id: non_manager.id}
+        expect(response).to render_template "shared/404"
       end
     end
 
-    context 'when warehouse is invalid' do
-      before(:each) do
-        post :create, params: {warehouse: {name: nil}}
-      end
-      it 'assigns an errors variable' do
-        expect(assigns[:errors]).to_not be_empty
-      end
-      it 'renders the new warehouse view' do
-        expect(response).to render_template(:new)
-      end
-    end
   end
 end
